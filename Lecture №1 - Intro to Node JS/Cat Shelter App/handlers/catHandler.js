363fb6635aa00fe7a7a5a9e60a881d9657affe92
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2021-01-15 13:45:53 +0200

    Added add-breed functionality.

--- Lecture №1 - Intro to Node JS/Cat Shelter App/handlers/catHandler.js
+++ Lecture №1 - Intro to Node JS/Cat Shelter App/handlers/catHandler.js
@@ -8,49 +8,87 @@ const cats = require('../data/cats.json');
 
 module.exports = (req, res) => {
 
-    const pathname = url.parse(req.url).pathname;
+    const parsedUrl = url.parse(req.url);
+    const pathname = parsedUrl.pathname;
 
-    if (pathname === '/cats/add-cat' && req.method === 'GET') {
+    if (pathname === '/cats/add-cat') {
+        if (req.method === 'GET') {
+            const filePath = './views/addCat.html';
 
-        const filePath = './views/addCat.html';
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.log(err);
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-                
-                res.writeHead(404, {
-                    'Content-Type': 'text/html',
-                });
-                res.write('<h1 style="text-align: center">404 Page Not Found </h1>');
-                res.end();
-            } else {
-                res.writeHead(200, {
-                    'Content-Type': 'text/html',
-                });
-                res.write(data);
-                res.end();
-            }
-        });
-    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
-        
-        const filePath = './views/addBreed.html';
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write('<h1 style="text-align: center">404 Page Not Found </h1>');
+                    res.end();
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write(data);
+                    res.end();
+                }
+            });
+        } else if (req.method === 'POST') {
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-                
-                res.writeHead(404, {
-                    'Content-Type': 'text/html',
+        }
+    } else if (pathname === '/cats/add-breed') {
+        if (req.method === 'GET') {
+            const filePath = './views/addBreed.html';
+
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.log(err);
+
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write('<h1 style="text-align: center">404 Page Not Found </h1>');
+                    res.end();
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write(data);
+                    res.end();
+                }
+            });
+        } else if (req.method === 'POST') {
+            const form = new formidable.IncomingForm();
+            const filePath = './data/breeds.json';
+            let inputBreed;
+            let catBreeds;
+
+            form.parse(req, function (err, fields, files) {
+                if (err) {
+                    console.log(err);
+
+                }
+
+                inputBreed = fields.breed;
+
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    }
+
+                    catBreeds = JSON.parse(data);
+
+                    catBreeds.push(inputBreed);
+
+                    fs.writeFile(filePath, JSON.stringify(catBreeds), (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
                 });
-                res.write('<h1 style="text-align: center">404 Page Not Found </h1>');
+
                 res.end();
-            } else {
-                res.writeHead(200, {
-                    'Content-Type': 'text/html',
-                });
-                res.write(data);
-                res.end();
-            }
-        });
+            });
+
+        }
     }
 };
