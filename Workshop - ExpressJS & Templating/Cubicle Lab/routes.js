6248b71942e8fd5a76602632e4167392d161078e
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2021-01-28 16:27:37 +0200

    commit

---
+++ Workshop - ExpressJS & Templating/Cubicle Lab/routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router();
+const productController = require('./controllers/productController');
+const aboutController = require('./controllers/aboutController');
+
+router.use('/', productController);
+router.use('/about', aboutController);
+
+router.get('*', (req, res) => {
+    res.render('404', { layout: false })
+});
+
+module.exports = router;
